bce46bb0e0d877f3b2d050f1e40090fc1c0de477
Sanjay Singh Shahi <shahi.sanjayshahi@gmail.com>
2024-08-27 10:46:07 +0530

    remove unwanted code

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import { defineAsyncComponent } from 'vue';
+// import { defineAsyncComponent } from 'vue';
 
 import CoachesList from './pages/coaches/CoachesList.vue';
 import CoachDetails from './pages/coaches/CoachDetails.vue';
